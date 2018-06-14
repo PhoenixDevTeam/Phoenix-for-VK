f89429574f2805b4fafdb8288f7d24fb3c13bd3b
dealnote.dev <dealnote.dev@gmail.com>
2018-06-14 14:16:44 +0300

    upload experiments

---
+++ app/src/main/java/biz/dealnote/messenger/upload/experimental/UploadResult.java
@@ -0,0 +1,22 @@
+package biz.dealnote.messenger.upload.experimental;
+
+import biz.dealnote.messenger.api.model.server.UploadServer;
+
+public class UploadResult<T> {
+
+    private final UploadServer server;
+    private final T result;
+
+    public UploadResult(UploadServer server, T result) {
+        this.server = server;
+        this.result = result;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public UploadServer getServer() {
+        return server;
+    }
+}
