2e0775070a5eee94d2b4bd85b623ef25f3f8f584
Emin <goldenemin@gmail.com>
2018-10-26 13:25:38 +0300

    Merging with kotlin

---
+++ app/src/main/java/biz/dealnote/messenger/db/model/MessagePatch.kt
@@ -0,0 +1,10 @@
+package biz.dealnote.messenger.db.model
+
+class MessagePatch(val messageId: Int) {
+    var deletion: Deletion? = null
+    var important: Important? = null
+
+    class Deletion(val deleted: Boolean)
+
+    class Important(val important: Boolean)
+}
